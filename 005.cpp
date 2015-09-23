878f085c183bd48e6ae269cdd0367e9c95938ec8
Haochen Liu <liuhaochen1221@hotmail.com>
2015-09-22 17:24:27 -0700

    Create 005.cpp

---
+++ 005.cpp
@@ -0,0 +1,49 @@
+/*
+005. Longest Palindromic Substring
+O(n) 有难度
+动态规划
+
+http://blog.csdn.net/linulysses/article/details/5634104
+Manacher's ALGORITHM: O(n)时间求字符串的最长回文子串
+http://www.felix021.com/blog/read.php?2040
+http://leetcode.com/2011/11/longest-palindromic-substring-part-ii.html
+
+Given a string S, find the longest palindromic substring in S. You may assume that the maximum length of S is 1000, and there exists one unique longest palindromic substring.
+
+O(n)
+*/
+
+class Solution {
+public:
+    string longestPalindrome(string s) {
+        int n = s.size();
+        if(n < 2) return s;
+        string str = "#";
+        for(int i = 0; i < s.size(); ++i) {
+            str.push_back(s[i]);
+            str.push_back('#');
+        }
+        vector<int> dp(str.size(), 0);
+        int R = 0;
+        int C = 0;
+        int len = 0;
+        for(int i = 0; i < dp.size(); ++i) {
+            if(i < R) {
+                dp[i] = min(dp[2 * C - i], R- i);
+            } else {
+                dp[i] = 1;
+            }
+            while(i >= dp[i] && i + dp[i] < dp.size() && str[i + dp[i]] == str[i - dp[i]]) {
+                dp[i]++;
+            }
+            dp[i]--;
+            
+            if(dp[i] > len) {
+                len = dp[i];
+                C = i;
+                R = i + dp[i];
+            }
+        }
+        return s.substr((C - dp[C]) / 2, dp[C]);
+    }
+};
