e0907574f617e0550b47eb5b9c88fb6b53365d87
Haochen Liu <liuhaochen1221@hotmail.com>
2015-09-22 20:05:54 -0700

    Create 121.cpp

---
+++ 121.cpp
@@ -0,0 +1,24 @@
+/*
+121. Best Time to Buy and Sell Stock
+http://www.cnblogs.com/remlostime/archive/2012/11/06/2757434.html
+
+Say you have an array for which the ith element is the price of a given stock on day i.
+
+If you were only permitted to complete at most one transaction (ie, buy one and sell one share of the stock), design an algorithm to find the maximum profit.
+*/
+
+class Solution {
+public:
+    int maxProfit(vector<int> &prices) {
+        if (prices.size() == 0) return 0;
+            
+        int maxPrice = prices[prices.size() - 1];
+        int ans = 0;
+        for(int i = prices.size() - 1; i >= 0; i--) {
+            maxPrice = max(maxPrice, prices[i]);
+            ans = max(ans, maxPrice - prices[i]);
+        }
+        
+        return ans;
+    }
+};
