83314a5e33fae43f43d2654f8f4917a945a96261
Haochen Liu <liuhaochen1221@hotmail.com>
2015-09-22 18:55:25 -0700

    Create 050.cpp

---
+++ 050.cpp
@@ -0,0 +1,29 @@
+/*
+050. Pow(x, n)
+
+Implement pow(x, n).
+*/
+
+class Solution {
+public:
+    double myPow(double x, int n) {
+        if(x == 0.0) return 0.0;
+        if(x == 1.0) return 1.0;
+        if(x < 0 && n % 2 == 1) return -myPow(-x, n);
+        if(n == 0) return 1;
+        if(n == 1) return x;
+        if(n < 0) {
+            if(n != INT_MIN) {
+                return 1 / myPow(x, -n);
+            } else {
+                return 1 / myPow(x, INT_MAX) / x;
+            }
+        }
+        double t = myPow(x, n / 2);
+        if(n % 2 == 0) {
+            return t * t;
+        } else {
+            return t * t * x;
+        }
+    }
+};
