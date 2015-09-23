73fe2c4b36bddb89bb697aec0140baf0be857568
Haochen Liu <liuhaochen1221@hotmail.com>
2015-09-22 18:55:52 -0700

    Create 050-2.cpp

---
+++ 050-2.cpp
@@ -0,0 +1,31 @@
+/*
+050. Pow(x, n)
+
+Implement pow(x, n).
+*/
+
+class Solution {
+public:
+    double power(double x, int n) {
+        if(n == 0) return 1.0;
+        double t = power(x, n / 2);
+        if(n % 2 == 0) {
+            return t * t;
+        } else {
+            return t * t * x;
+        }
+    }
+    
+    double myPow(double x, int n) {
+        if(n >= 0) {
+            return power(x, n);
+        } else {
+            double t = 1.0 / power(x, n / 2);
+            if(n % 2 == 0) {
+                return t * t;
+            } else {
+                return t * t / x;
+            }
+        }
+    }
+};
