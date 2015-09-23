2e6130959b005ca4a46af6525e52d7dac7d6aa39
Haochen Liu <liuhaochen1221@hotmail.com>
2015-09-22 18:53:39 -0700

    Create 048-1.cpp

---
+++ 048-1.cpp
@@ -0,0 +1,35 @@
+/*
+048. Rotate Image
+
+You are given an n x n 2D matrix representing an image.
+
+Rotate the image by 90 degrees (clockwise).
+
+Follow up:
+Could you do this in-place?
+*/
+
+class Solution {
+public:
+    void rotate(vector<vector<int> > &matrix) {
+        int n = matrix.size();
+        if(n == 0) return;
+        for(int i = 0; i < n; i++) {
+            for(int j = i + 1; j < n; j++) {
+                int t = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = t;
+            }
+        }
+        
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n / 2; j++) {
+                int t = matrix[i][j];
+                matrix[i][j] = matrix[i][n - 1 - j];
+                matrix[i][n - 1 - j] = t;
+            }
+        }
+        
+        return;
+    }
+};
