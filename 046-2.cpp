d6c5538d0221e8126723f56e1c26d363cfc3ef32
Haochen Liu <liuhaochen1221@hotmail.com>
2015-09-22 18:52:33 -0700

    Create 046-2.cpp

---
+++ 046-2.cpp
@@ -0,0 +1,37 @@
+/*
+046. Permutations
+
+Given a collection of numbers, return all possible permutations.
+
+For example,
+[1,2,3] have the following permutations:
+[1,2,3], [1,3,2], [2,1,3], [2,3,1], [3,1,2], and [3,2,1].
+*/
+
+class Solution {
+private:
+    void dfs(vector<vector<int>>& res, vector<int>& record, vector<int>& nums, int index) {
+        int size = nums.size();
+        if(index == size) {
+            res.push_back(record);
+            return;
+        }
+        
+        for(int i = index; i < size; i++) {
+            swap(nums[index], nums[i]);
+            record.push_back(nums[index]);
+            dfs(res, record, nums, index + 1);
+            record.pop_back();
+            swap(nums[index], nums[i]);
+        }
+    }
+    
+public:
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>> res;
+        if(nums.size() == 0) return res;
+        vector<int> record;
+        dfs(res, record, nums, 0);
+        return res;
+    }
+};
