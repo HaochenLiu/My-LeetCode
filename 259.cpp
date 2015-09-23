fb54509d9a62330c3b5c7a7fa3cb5ea7385edfba
Haochen Liu <liuhaochen1221@hotmail.com>
2015-09-23 00:33:50 -0700

    Create 259.cpp

---
+++ 259.cpp
@@ -0,0 +1,44 @@
+/*
+259. 3Sum Smaller
+
+Given an array of n integers nums and a target, find the number of index triplets i, j, k with 0 <= i < j < k < n that satisfy the condition nums[i] + nums[j] + nums[k] < target.
+For example, given nums = [-2, 0, 1, 3], and target = 2.
+Return 2. Because there are two triplets which sums are less than 2:
+[-2, 0, 1]
+[-2, 0, 3]
+
+
+Follow up:
+Could you solve it in O(n2) runtime?
+
+Given n nodes labeled from 0 to n - 1 and a list of undirected edges (each edge is a pair of nodes), write a function to check whether these edges make up a valid tree.
+For example:
+Given n = 5 and edges = [[0, 1], [0, 2], [0, 3], [1, 4]], return true.
+Given n = 5 and edges = [[0, 1], [1, 2], [2, 3], [1, 3], [1, 4]], return false.
+Hint:
+Given n = 5 and edges = [[0, 1], [1, 2], [3, 4]], what should your return? Is this case a valid tree?
+According to the definition of tree on Wikipedia: “a tree is an undirected graph in which any two vertices are connected by exactly one path. In other words, any connected graph without simple cycles is a tree.”
+Note: you can assume that no duplicate edges will appear in edges. Since all edges are undirected, [0, 1] is the same as [1, 0] and thus will not appear together in edges.
+*/
+
+class Solution {
+public:
+    int threeSumSmaller(vector<int>& nums, int target) {
+        sort(nums.begin(), nums.end());
+        int n = nums.size();
+        int ans = 0;
+        for(int i = 0; i < n - 2; i++) {
+            int j = i + 1;
+            int k = n - 1;
+            while(j < k) {
+                if (nums[i] + nums[j] + nums[k] >= target) {
+                    k--;
+                } else {
+                    ans += (k - j);
+                    j++;
+                }
+            }
+        }
+        return ans;
+    }
+};
