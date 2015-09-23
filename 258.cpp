4030c58e00474c0faa87e461728c3142ea4ba03f
Haochen Liu <liuhaochen1221@hotmail.com>
2015-09-23 00:33:05 -0700

    Create 258.cpp

---
+++ 258.cpp
@@ -0,0 +1,29 @@
+/*
+258. Add Digits
+
+Given a non-negative integer num, repeatedly add all its digits until the result has only one digit.
+For example:
+Given num = 38, the process is like: 3 + 8 = 11, 1 + 1 = 2. Since 2 has only one digit, return it.
+Follow up:
+Could you do it without any loop/recursion in O(1) runtime?
+Hint:
+A naive implementation of the above process is trivial. Could you come up with other methods?
+What are all the possible results?
+How do they occur, periodically or randomly?
+You may find this Wikipedia article useful.
+Credits:
+Special thanks to @jianchao.li.fighter for adding this problem and creating all test cases.
+*/
+
+class Solution {
+public:
+    int addDigits(int num) {
+        if(num == 0) {
+            return 0;
+        } else if(num % 9 == 0) {
+            return 9;
+        } else {
+            return num % 9;
+        }
+    }
+};
