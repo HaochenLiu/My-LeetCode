5f96ca40583f70735497e705582346d188ff1c71
Haochen Liu <liuhaochen1221@hotmail.com>
2015-09-22 18:45:21 -0700

    Create 036-2.cpp

---
+++ 036-2.cpp
@@ -0,0 +1,36 @@
+/*
+036. Valid Sudoku
+
+Determine if a Sudoku is valid, according to: Sudoku Puzzles - The Rules.
+
+The Sudoku board could be partially filled, where empty cells are filled with the character '.'.
+
+A partially filled sudoku which is valid.
+
+Note:
+A valid Sudoku board (partially filled) is not necessarily solvable. Only the filled cells need to be validated.
+*/
+
+class Solution {
+public:
+    bool isValidSudoku(vector<vector<char>> &board) {
+        const int N = 9;
+        int col[N];
+        int box[N];
+        memset(col, 0, sizeof(col));
+        memset(box, 0, sizeof(box));
+        for(int i = 0; i < N; ++i) {
+            int row = 0;
+            for(int j = 0; j < N; ++j) {
+                if(board[i][j] == '.') continue;
+                int bit = 1 << (board[i][j] - '1');
+                int box_index = i / 3 * 3 + j / 3;
+                if((row & bit) || (col[j] & bit) || (box[box_index] & bit)) return false;
+                row |= bit;
+                col[j] |= bit;
+                box[box_index] |= bit;
+            }
+        }
+        return true;
+    }
+};
