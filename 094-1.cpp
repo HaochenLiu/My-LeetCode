fb2826a606b0ee271b0817a64d2eda7953ffa466
Haochen Liu <liuhaochen1221@hotmail.com>
2015-09-22 19:38:02 -0700

    Create 094-1.cpp

---
+++ 094-1.cpp
@@ -0,0 +1,42 @@
+/*
+094. Binary Tree Inorder Traversal
+http://www.cnblogs.com/TenosDoIt/p/3445449.html
+
+Given a binary tree, return the inorder traversal of its nodes' values.
+
+For example:
+Given binary tree {1,#,2,3},
+
+  1
+    \
+     2
+    /
+   3
+
+return [1,3,2].
+
+Note: Recursive solution is trivial, could you do it iteratively?
+
+confused what "{1,#,2,3}" means? > read more on how binary tree is serialized on OJ.
+*/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+private:
+    vector<int> res;
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        if(root && root->left) inorderTraversal(root->left);
+        if(root) res.push_back(root->val);
+        if(root && root->right) inorderTraversal(root->right);
+        return res;
+    }
+};
