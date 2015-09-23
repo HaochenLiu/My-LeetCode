5d0e816e4552d95c3c447b92f0c96b3334de21fd
Haochen Liu <liuhaochen1221@hotmail.com>
2015-09-22 17:33:14 -0700

    Create 014.cpp

---
+++ 014.cpp
@@ -0,0 +1,29 @@
+/*
+014. Longest Common Prefix
+O(n)
+BF
+*/
+
+Write a function to find the longest common prefix string amongst an array of strings. 
+
+class Solution {
+public:
+    string findCommon(string s1, string s2) {
+        int n = min(s1.length(), s2.length());
+        for(int i = 0; i < n; ++i) {
+            if(s1[i] != s2[i]) return s1.substr(0, i);
+        }
+        return s1.substr(0, n);
+    }
+
+    string longestCommonPrefix(vector<string> &strs) {
+        if(strs.size() == 0) return "";
+        if(strs.size() == 1) return strs[0];
+
+        string res = strs[0];
+        for(int i = 1; i < strs.size(); ++i) {
+            res = findCommon(res, strs[i]);
+        }
+        return res;
+    }
+};
