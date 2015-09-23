39634dc216bc8ccdf46df3f229ffec31dac01030
Haochen Liu <liuhaochen1221@hotmail.com>
2015-09-22 18:42:58 -0700

    Create 033.cpp

---
+++ 033.cpp
@@ -0,0 +1,37 @@
+/*
+033. Search in Rotated Sorted Array
+O(logn)
+
+Suppose a sorted array is rotated at some pivot unknown to you beforehand.
+(i.e., 0 1 2 4 5 6 7 might become 4 5 6 7 0 1 2).
+You are given a target value to search. If found in the array return its index, otherwise return -1.
+You may assume no duplicate exists in the array.
+*/
+
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int n = nums.size();
+        int l = 0;
+        int r = n - 1;
+        int m;
+        while(l <= r) {
+            m = (l + r) / 2;
+            if(nums[m] == target) return m;
+            if(nums[m] >= nums[l]) {
+                if(nums[l] <= target && target < nums[m]) {
+                    r = m - 1;
+                } else {
+                    l = m + 1;
+                }
+            } else {
+                if(nums[m] < target && target <= nums[r]) {
+                    l = m + 1;
+                } else {
+                    r = m - 1;
+                }
+            }
+        }
+        return -1;
+    }
+};
