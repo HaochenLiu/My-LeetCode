1083fa61149b47cfb1160e8b0343121f2a5e4b92
Haochen Liu <liuhaochen1221@hotmail.com>
2015-09-22 18:20:36 -0700

    Create 021-1.cpp

---
+++ 021-1.cpp
@@ -0,0 +1,44 @@
+/*
+021. Merge Two Sorted Lists
+
+Merge two sorted linked lists and return it as a new list. The new list should be made by splicing together the nodes of the first two lists.
+*/
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        ListNode dummy(0);
+        ListNode* head = &dummy;
+        ListNode* cur = head;
+        while(l1 || l2) {
+            if(!l1) {
+                cur->next = l2;
+                break;
+            }
+            
+            if(!l2) {
+                cur->next = l1;
+                break;
+            }
+            
+            if(l1->val < l2->val) {
+                cur->next = l1;
+                l1 = l1->next;
+                cur = cur->next;
+            } else {
+                cur->next = l2;
+                l2 = l2->next;
+                cur = cur->next;
+            }
+        }
+        return head->next;
+    }
+};
