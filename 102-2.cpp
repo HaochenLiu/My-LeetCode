fff8daae8c053cb914e36f1138cdf73fc6f382b2
Haochen Liu <liuhaochen1221@hotmail.com>
2015-09-22 19:43:54 -0700

    Create 102-2.cpp

---
+++ 102-2.cpp
@@ -0,0 +1,63 @@
+/*
+102. Binary Tree Level Order Traversal
+
+Given a binary tree, return the level order traversal of its nodes' values. (ie, from left to right, level by level).
+
+For example:
+Given binary tree {3,9,20,#,#,15,7},
+
+   3
+   / \
+  9  20
+    /  \
+   15   7
+
+return its level order traversal as:
+
+[
+  [3],
+  [9,20],
+  [15,7]
+]
+
+confused what "{1,#,2,3}" means? > read more on how binary tree is serialized on OJ.
+*/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> levelOrder(TreeNode* root) {
+        vector<vector<int>> res;
+        vector<int> record;
+        if(root == NULL) return res;
+        queue<TreeNode*> current;
+        queue<TreeNode*> next;
+        current.push(root);
+        while(!current.empty()) {
+            while(!current.empty()) {
+                TreeNode* node = current.front();
+                current.pop();
+                record.push_back(node->val);
+                if(node->left) {
+                    next.push(node->left);
+                }
+                if(node->right) {
+                    next.push(node->right);
+                }
+            }
+            res.push_back(record);
+            record.clear();
+            swap(current, next);
+        }
+
+        return res;
+    }
+};
