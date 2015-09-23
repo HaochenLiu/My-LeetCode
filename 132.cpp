a0b32ca2b6df5d3b63307d744de41d23c62a0f68
Haochen Liu <liuhaochen1221@hotmail.com>
2015-09-22 20:16:09 -0700

    Create 132.cpp

---
+++ 132.cpp
@@ -0,0 +1,43 @@
+/*
+132. Palindrome Partitioning II
+
+Given a string s, partition s such that every substring of the partition is a palindrome.
+Return the minimum cuts needed for a palindrome partitioning of s.
+For example, given s = "aab",
+Return 1 since the palindrome partitioning ["aa","b"] could be produced using 1 cut.
+*/
+
+class Solution{
+public:
+    int minCut(string s) {
+        int n = s.length();
+        if(n < 2) return 0;
+        
+        int* dp = new int[n + 1];
+        for(int i = 0; i <= n; ++i) {
+            dp[i] = n - i;
+        }
+        
+        bool** m = new bool*[n];
+        for(int i = 0; i < n; ++i) {
+            m[i] = new bool[n];
+            memset(m[i], 0, sizeof(bool) * n);
+        }
+        
+        for(int beg = n - 1; beg >= 0; --beg) {
+            for(int end = beg; end < n; ++end) {
+                if(s[beg] == s[end] && (end - beg < 2 || m[beg + 1][end - 1])) {
+                    m[beg][end] = true;
+                    dp[beg] = min(dp[beg], dp[end + 1] + 1);
+                }
+            }
+        }
+        
+        for(int i = 0; i < n; ++i) {
+            delete m[i];
+        }
+        delete m;
+        
+        return dp[0] - 1;
+    }
+};
