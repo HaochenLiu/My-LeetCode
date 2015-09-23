7b572aa5a7026ab3c8f173823b306d3620793db7
Haochen Liu <liuhaochen1221@hotmail.com>
2015-09-22 19:05:06 -0700

    Create 056.cpp

---
+++ 056.cpp
@@ -0,0 +1,42 @@
+/*
+056. Merge Intervals
+
+Given a collection of intervals, merge all overlapping intervals.
+
+For example,
+Given [1,3],[2,6],[8,10],[15,18],
+return [1,6],[8,10],[15,18].
+*/
+
+/**
+ * Definition for an interval.
+ * struct Interval {
+ *     int start;
+ *     int end;
+ *     Interval() : start(0), end(0) {}
+ *     Interval(int s, int e) : start(s), end(e) {}
+ * };
+ */
+
+bool comp(Interval a, Interval b) {
+    return a.start < b.start;
+}
+
+class Solution {
+public:
+    vector<Interval> merge(vector<Interval>& intervals) {
+        int n = intervals.size();
+        if(n < 2) return intervals;
+        vector<Interval> res;
+        sort(intervals.begin(), intervals.end(), comp);
+        res.push_back(intervals[0]);
+        for(int i = 1; i < n; i++) {
+            if(res.back().end >= intervals[i].start) {
+                res.back().end = max(res.back().end, intervals[i].end);
+            } else {
+                res.push_back(intervals[i]);
+            }
+        }
+        return res;
+    }
+};
