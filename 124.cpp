41f354315a42adc3289c43a95e236c272e833ffc
Haochen Liu <liuhaochen1221@hotmail.com>
2015-09-22 20:08:39 -0700

    Create 124.cpp

---
+++ 124.cpp
@@ -0,0 +1,43 @@
+/*
+124. Binary Tree Maximum Path Sum
+
+Given a binary tree, find the maximum path sum.
+
+The path may start and end at any node in the tree.
+
+For example:
+Given the below binary tree,
+
+      1
+      / \
+     2   3
+
+Return 6.
+*/
+
+/**
+ * Definition for binary tree
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    int maxPathSum(TreeNode* root) {
+        int res = INT_MIN;
+        maxPathSumRe(root, res);
+        return res;
+    }
+    
+    int maxPathSumRe(TreeNode* node, int &res) {
+        if (!node) return 0;
+        int l = maxPathSumRe(node->left, res);
+        int r = maxPathSumRe(node->right, res);
+        int sum = max(node->val, max(l, r) + node->val);
+        res = max(res, max(0, l) + max(0, r) + node->val);
+        return sum;
+    }
+};
