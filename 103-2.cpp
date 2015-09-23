020afd69832c35b9f83164c5b8cd64fc53895084
Haochen Liu <liuhaochen1221@hotmail.com>
2015-09-22 19:45:12 -0700

    Create 103-2.cpp

---
+++ 103-2.cpp
@@ -0,0 +1,66 @@
+/*
+103. Binary Tree Zigzag Level Order Traversal
+
+Given a binary tree, return the zigzag level order traversal of its nodes' values. (ie, from left to right, then right to left for the next level and alternate between).
+
+For example:
+Given binary tree {3,9,20,#,#,15,7},
+
+   3
+   / \
+  9  20
+    /  \
+   15   7
+
+return its zigzag level order traversal as:
+
+[
+  [3],
+  [20,9],
+  [15,7]
+]
+
+confused what "{1,#,2,3}" means? > read more on how binary tree is serialized on OJ.
+*/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
+        vector<vector<int>> res;
+        vector<int> record;
+        if(root == NULL) return res;
+        queue<TreeNode*> current;
+        queue<TreeNode*> next;
+        current.push(root);
+        while(!current.empty()) {
+            while(!current.empty()) {
+                TreeNode* node = current.front();
+                current.pop();
+                record.push_back(node->val);
+                if(node->left) {
+                    next.push(node->left);
+                }
+                if(node->right) {
+                    next.push(node->right);
+                }
+            }
+            res.push_back(record);
+            record.clear();
+            swap(current, next);
+        }
+        for(int i = 1; i < res.size(); i += 2) {
+            reverse(res[i].begin(), res[i].end());
+        }
+
+        return res;
+    }
+};
