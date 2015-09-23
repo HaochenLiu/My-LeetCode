7549dd8abc0d4adfe6d2495f0491d7aa5c030776
Haochen Liu <liuhaochen1221@hotmail.com>
2015-09-22 23:10:47 -0700

    Create 176.sql

---
+++ 176.sql
@@ -0,0 +1,27 @@
+176. Second Highest Salary
+
+Write a SQL query to get the second highest salary from the Employee table.
++----+--------+
+| Id | Salary |
++----+--------+
+| 1  | 100    |
+| 2  | 200    |
+| 3  | 300    |
++----+--------+
+
+
+For example, given the above Employee table, the second highest salary is 200. If there is no second highest salary, then the query should return null.
+
+select MAX(Salary) from Employee;
+
+# Write your MySQL query statement below
+select MAX(Salary) from Employee
+WHERE Salary <> (select MAX(Salary) from Employee)
+
+# Write your MySQL query statement below
+select MAX(Salary) from Employee
+WHERE Salary < (select MAX(Salary) from Employee)
+
+# Write your MySQL query statement below
+SELECT MAX(Salary) FROM Employee
+WHERE Salary NOT IN (SELECT MAX(Salary) FROM Employee)
