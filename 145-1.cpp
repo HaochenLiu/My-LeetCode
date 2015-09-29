f41350ccdee02af02dff306fa3632959fcd1f7b4
Haochen Liu <liuhaochen1221@hotmail.com>
2015-09-29 01:54:54 -0700

    Update 145-1.cpp

--- 145-1.cpp
+++ 145-1.cpp
@@ -17,6 +17,13 @@ return [3,2,1].
 Note: Recursive solution is trivial, could you do it iteratively?
 */
 
+/*
+Recursion
+Time: O(n)
+Space: O(h)
+h is depth of the tree. 
+*/
+
 /**
  * Definition for a binary tree node.
  * struct TreeNode {
