3a59c7fb5652aaaa49951e3151e6e4beded39c55
Haochen Liu <liuhaochen1221@hotmail.com>
2015-09-29 22:41:29 -0700

    Update 021-1.cpp

--- 021-1.cpp
+++ 021-1.cpp
@@ -4,6 +4,12 @@
 Merge two sorted linked lists and return it as a new list. The new list should be made by splicing together the nodes of the first two lists.
 */
 
+/*
+Time: O(n)
+Space: O(1)
+n is number of nodes
+*/
+
 /**
  * Definition for singly-linked list.
  * struct ListNode {
