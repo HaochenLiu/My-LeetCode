a06b6c0426a4ddf1fcb42a63fa7a56627ef79e20
Haochen Liu <liuhaochen1221@hotmail.com>
2015-09-30 14:47:08 -0700

    Update 280.cpp

--- 280.cpp
+++ 280.cpp
@@ -5,6 +5,12 @@ Given an unsorted array nums, reorder it in-place such that nums[0] <= nums[1] >
 For example, given nums = [3, 5, 2, 1, 6, 4], one possible answer is [1, 6, 2, 5, 3, 4].
 */
 
+/*
+Time: O(n)
+Space: O(1)
+Extra space: O(1)
+*/
+
 class Solution {
 public:
     void wiggleSort(vector<int>& nums) {
