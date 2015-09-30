775be2f67317151d6d292b0756008df37568b02d
Haochen Liu <liuhaochen1221@hotmail.com>
2015-09-29 22:27:49 -0700

    Update 017.cpp

--- 017.cpp
+++ 017.cpp
@@ -1,18 +1,21 @@
 /*
 017. Letter Combinations of a Phone Number
-BF
 
 Given a digit string, return all possible letter combinations that the number could represent.
-
 A mapping of digit to letters (just like on the telephone buttons) is given below.
-
-
 
 Input:Digit string "23"
 Output: ["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"].
 
 Note:
 Although the above answer is in lexicographical order, your answer could be in any order you want.
+*/
+
+/*
+Time: O(4^n)
+Space: O(4^n)
+Extra space: O(1)
+n is the lenght of digits
 */
 
 class Solution {
