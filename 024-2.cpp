f5cd189d7d3d7c7509b5c19bb6385cca011e4af6
Haochen Liu <liuhaochen1221@hotmail.com>
2015-09-29 22:49:26 -0700

    Update 024-2.cpp

--- 024-2.cpp
+++ 024-2.cpp
@@ -1,13 +1,16 @@
 /*
 024. Swap Nodes in Pairs
-O(n)
 
 Given a linked list, swap every two adjacent nodes and return its head.
-
 For example,
 Given 1->2->3->4, you should return the list as 2->1->4->3.
-
 Your algorithm should use only constant space. You may not modify the values in the list, only nodes itself can be changed.
+*/
+
+/*
+Time: O(n)
+Space: O(1)
+n is number of nodes
 */
 
 /**
