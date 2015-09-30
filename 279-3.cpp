a598fa57ec0d6ab1167a34d4221734c29dfc984c
Haochen Liu <liuhaochen1221@hotmail.com>
2015-09-30 14:48:36 -0700

    Update 279-3.cpp

--- 279-3.cpp
+++ 279-3.cpp
@@ -7,6 +7,12 @@ Credits:
 Special thanks to @jianchao.li.fighter for adding this problem and creating all test cases.
 */
 
+/*
+Time: O(n)
+Space: O(n)
+Extra space: O(n)
+*/
+
 class Solution {
 private:
     int getNum(vector<int>& dp, int n) {
