a9a07ac9ac281d3917c2cb8bea9a9d5cb62f543c
Haochen Liu <liuhaochen1221@hotmail.com>
2015-09-30 11:30:03 -0700

    Update 042.cpp

--- 042.cpp
+++ 042.cpp
@@ -9,6 +9,12 @@ Given [0,1,0,2,1,0,1,3,2,1,2,1], return 6.
 The above elevation map is represented by array [0,1,0,2,1,0,1,3,2,1,2,1]. In this case, 6 units of rain water (blue section) are being trapped. Thanks Marcos for contributing this image!
 */
 
+/*
+Time: O(n)
+Space: O(1)
+Extra space: O(1)
+*/
+
 class Solution {
 public:
     int trap(vector<int>& height) {
