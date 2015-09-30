d19e10251c11868b42d2c44db91b30642bca351e
Haochen Liu <liuhaochen1221@hotmail.com>
2015-09-30 09:48:23 -0700

    Update 035.cpp

--- 035.cpp
+++ 035.cpp
@@ -1,6 +1,5 @@
 /*
 035. Search Insert Position
-O(logn)
 
 Given a sorted array and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
 You may assume no duplicates in the array.
@@ -9,6 +8,13 @@ Here are few examples.
 [1,3,5,6], 2 → 1
 [1,3,5,6], 7 → 4
 [1,3,5,6], 0 → 0
+*/
+
+/*
+Time: O(logn)
+Space: O(1)
+Extra space: O(1)
+n is length of the array.
 */
 
 class Solution {
