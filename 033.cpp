63283db149d48d77f0706fb714522d4150e5822c
Haochen Liu <liuhaochen1221@hotmail.com>
2015-09-30 09:47:26 -0700

    Update 033.cpp

--- 033.cpp
+++ 033.cpp
@@ -1,11 +1,17 @@
 /*
 033. Search in Rotated Sorted Array
-O(logn)
 
 Suppose a sorted array is rotated at some pivot unknown to you beforehand.
 (i.e., 0 1 2 4 5 6 7 might become 4 5 6 7 0 1 2).
 You are given a target value to search. If found in the array return its index, otherwise return -1.
 You may assume no duplicate exists in the array.
+*/
+
+/*
+Time: O(logn)
+Space: O(1)
+Extra space: O(1)
+n is length of the array.
 */
 
 class Solution {
