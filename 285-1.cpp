e355446a0c8f7793029ef9a26ad67b0634d12183
Haochen Liu <liuhaochen1221@hotmail.com>
2015-09-30 10:13:22 -0700

    Update 285-1.cpp

--- 285-1.cpp
+++ 285-1.cpp
@@ -5,6 +5,13 @@ Given a binary search tree and a node in it, find the in-order successor of that
 Note: If the given node has no in-order successor in the tree, return null.
 */
 
+/*
+Time: O(logn)
+Space: O(logn)
+Extra space: O(logn)
+Assume BST is balanced.
+*/
+
 /**
  * Definition for a binary tree node.
  * struct TreeNode {
