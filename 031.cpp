7413cf619c321df408ce0f516150f2dad1d4bc59
Haochen Liu <liuhaochen1221@hotmail.com>
2015-09-30 09:40:06 -0700

    Update 031.cpp

--- 031.cpp
+++ 031.cpp
@@ -1,18 +1,21 @@
 /*
 031. Next Permutation
-O(n)
+
 http://fisherlei.blogspot.com/2012/12/leetcode-next-permutation.html
-
 Implement next permutation, which rearranges numbers into the lexicographically next greater permutation of numbers.
-
 If such arrangement is not possible, it must rearrange it as the lowest possible order (ie, sorted in ascending order).
-
 The replacement must be in-place, do not allocate extra memory.
-
 Here are some examples. Inputs are in the left-hand column and its corresponding outputs are in the right-hand column.
 1,2,3 → 1,3,2
 3,2,1 → 1,2,3
 1,1,5 → 1,5,1
+*/
+
+/*
+Time: O(n)
+Space: O(n)
+Extra space: O(1)
+n is the length of nums.
 */
 
 class Solution {
