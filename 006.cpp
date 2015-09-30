769f1136f5120c72ded81f9f5dab01fde5fbac1d
Haochen Liu <liuhaochen1221@hotmail.com>
2015-09-29 22:18:28 -0700

    Update 006.cpp

--- 006.cpp
+++ 006.cpp
@@ -1,7 +1,5 @@
 /*
 006. ZigZag Conversion
-O(n)
-画个图
 
 The string "PAYPALISHIRING" is written in a zigzag pattern on a given number of rows like this: (you may want to display this pattern in a fixed font for better legibility)
 
@@ -10,12 +8,15 @@ A P L S I I G
 Y   I   R
 
 And then read line by line: "PAHNAPLSIIGYIR"
-
 Write the code that will take a string and make this conversion given a number of rows:
-
 string convert(string text, int nRows);
-
 convert("PAYPALISHIRING", 3) should return "PAHNAPLSIIGYIR".
+*/
+
+/*
+Time: O(n)
+Space: O(n)
+n is length of input string
 */
 
 class Solution {
