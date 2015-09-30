e95545a49b6e4d6c0fe51a47b1982c2e43f086a4
Haochen Liu <liuhaochen1221@hotmail.com>
2015-09-30 09:26:39 -0700

    Update 027-2.cpp

--- 027-2.cpp
+++ 027-2.cpp
@@ -1,9 +1,15 @@
 /*
 027. Remove Element
-O(n)
 
 Given an array and a value, remove all instances of that value in place and return the new length.
 The order of elements can be changed. It doesn't matter what you leave beyond the new length.
+*/
+
+/*
+Time: O(n^2)
+Space: O(n)
+Extra space: O(1)
+n is the length of the array
 */
 
 class Solution {
