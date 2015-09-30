0d79a5fb9c8df101118ecc9e9df4bb6ec9f0e9c8
Haochen Liu <liuhaochen1221@hotmail.com>
2015-09-29 22:23:10 -0700

    Update 012.cpp

--- 012.cpp
+++ 012.cpp
@@ -1,12 +1,16 @@
 /*
 012. Integer to Roman
-O(n)
 
 Given an integer, convert it to a roman numeral.
 Input is guaranteed to be within the range from 1 to 3999.
 
 //http://zh.wikipedia.org/wiki/%E7%BD%97%E9%A9%AC%E6%95%B0%E5%AD%97
 //I（1）、V（5）、X（10）、L（50）、C（100）、D（500）、M（1000）
+*/
+
+/*
+Time: O(1)
+Space: O(1)
 */
 
 class Solution {
