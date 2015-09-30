936ce097749423ef860f3de21f771d73702525c0
Haochen Liu <liuhaochen1221@hotmail.com>
2015-09-29 22:16:46 -0700

    Update 003.cpp

--- 003.cpp
+++ 003.cpp
@@ -5,6 +5,12 @@ O(n)
 Given a string, find the length of the longest substring without repeating characters. For example, the longest substring without repeating letters for "abcabcbb" is "abc", which the length is 3. For "bbbbb" the longest substring is "b", with the length of 1.
 */
 
+/*
+Time: O(n)
+Space: O(1)
+256 is O(1)
+*/
+
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
