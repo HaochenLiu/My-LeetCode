c19f6308c7e057e50d0b34a013f879072c021b15
Haochen Liu <liuhaochen1221@hotmail.com>
2015-09-29 22:20:10 -0700

    Update 009.cpp

--- 009.cpp
+++ 009.cpp
@@ -1,20 +1,18 @@
 /*
 009. Palindrome Number
-O(n)
-负数不是回文数
 
 Determine whether an integer is a palindrome. Do this without extra space.
-
 click to show spoilers.
 Some hints:
-
 Could negative integers be palindromes? (ie, -1)
-
 If you are thinking of converting the integer to string, note the restriction of using extra space.
-
 You could also try reversing an integer. However, if you have solved the problem "Reverse Integer", you know that the reversed integer might overflow. How would you handle such case?
-
 There is a more generic way of solving this problem.
+*/
+
+/*
+Time: O(1)
+Space: O(1)
 */
 
 class Solution {
