88e70ad42a4226faac7bb80395f05710c3a03d89
Haochen Liu <liuhaochen1221@hotmail.com>
2015-09-29 22:28:40 -0700

    Update 018-1.cpp

--- 018-1.cpp
+++ 018-1.cpp
@@ -1,22 +1,25 @@
 /*
 018. 4Sum
+
 http://www.cnblogs.com/TenosDoIt/p/3649607.html
 
-O(n^3)
-
 Given an array S of n integers, are there elements a, b, c, and d in S such that a + b + c + d = target? Find all unique quadruplets in the array which gives the sum of target.
-
 Note:
-
     Elements in a quadruplet (a,b,c,d) must be in non-descending order. (ie, a ≤ b ≤ c ≤ d)
     The solution set must not contain duplicate quadruplets.
-
     For example, given array S = {1 0 -1 0 -2 2}, and target = 0.
-
     A solution set is:
     (-1,  0, 0, 1)
     (-2, -1, 1, 2)
     (-2,  0, 0, 2)
+*/
+
+/*
+2 pointers
+Time: O(n^3)
+Space: O(k)
+Extra space: O(1)
+k is number of solutions
 */
 
 class Solution {
