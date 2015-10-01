be419f7546551efc83f5a8504b2542bcc1f61f24
Haochen Liu <liuhaochen1221@hotmail.com>
2015-09-30 22:09:56 -0700

    Update 064.cpp

--- 064.cpp
+++ 064.cpp
@@ -2,8 +2,14 @@
 064. Minimum Path Sum
 
 Given a m x n grid filled with non-negative numbers, find a path from top left to bottom right which minimizes the sum of all numbers along its path.
-
 Note: You can only move either down or right at any point in time.
+*/
+
+/*
+DP
+Time: O(nm)
+Space: O(nm)
+Extra space: O(nm)
 */
 
 class Solution {
