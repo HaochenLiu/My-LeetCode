d727d6ecc490708d91315fe0faacb1ca7451daad
Haochen Liu <liuhaochen1221@hotmail.com>
2015-10-01 00:27:20 -0700

    Update 088-1.cpp

--- 088-1.cpp
+++ 088-1.cpp
@@ -6,6 +6,12 @@ Note:
 You may assume that nums1 has enough space (size that is greater or equal to m + n) to hold additional elements from nums2. The number of elements initialized in nums1 and nums2 are m and n respectively.
 */
 
+/*
+Time: O(n+m)
+Space: O(n+m)
+Extra space: O(1)
+*/
+
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
