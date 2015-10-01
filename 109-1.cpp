4a09407c834f47ba2a4a2300f99ffecaf6398872
Haochen Liu <liuhaochen1221@hotmail.com>
2015-10-01 00:59:20 -0700

    Update 109-1.cpp

--- 109-1.cpp
+++ 109-1.cpp
@@ -2,8 +2,12 @@
 109. Convert Sorted List to Binary Search Tree
 
 Given a singly linked list where elements are sorted in ascending order, convert it to a height balanced BST.
+*/
 
-O(n)
+/*
+Time: O(n)
+Space: O(n)
+Extra space: O(logn)
 */
 
 /**
