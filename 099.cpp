557366b089facabefe6745f0a58066901b2d932b
Haochen Liu <liuhaochen1221@hotmail.com>
2015-10-01 00:51:28 -0700

    Update 099.cpp

--- 099.cpp
+++ 099.cpp
@@ -1,9 +1,8 @@
 /*
 099. Recover Binary Search Tree
+
 http://www.cnblogs.com/xinsheng/p/3468310.html
-
 Two elements of a binary search tree (BST) are swapped by mistake.
-
 Recover the tree without changing its structure.
 Note:
 A solution using O(n) space is pretty straight forward. Could you devise a constant space solution?
