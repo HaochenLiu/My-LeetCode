77e619631baa67918df2e6a80546c6ac0eac4089
Haochen Liu <liuhaochen1221@hotmail.com>
2015-10-01 00:32:06 -0700

    Update 089.cpp

--- 089.cpp
+++ 089.cpp
@@ -2,9 +2,7 @@
 089. Gray Code
 
 The gray code is a binary numeral system where two successive values differ in only one bit.
-
 Given a non-negative integer n representing the total number of bits in the code, print the sequence of gray code. A gray code sequence must begin with 0.
-
 For example, given n = 2, return [0,1,3,2]. Its gray code sequence is:
 
 00 - 0
@@ -14,10 +12,14 @@ For example, given n = 2, return [0,1,3,2]. Its gray code sequence is:
 
 Note:
 For a given n, a gray code sequence is not uniquely defined.
-
 For example, [0,2,3,1] is also a valid gray code sequence according to the above definition.
-
 For now, the judge is able to judge based on one instance of gray code sequence. Sorry about that.
+*/
+
+/*
+Time: O(2^n)
+Space: O(2^n)
+Extra space: O(1)
 */
 
 class Solution {
