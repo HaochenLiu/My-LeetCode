41f61409366ce80f77962c3e89b6e4debb5ed8ea
Haochen Liu <liuhaochen1221@hotmail.com>
2015-10-01 01:02:25 -0700

    Update 111.cpp

--- 111.cpp
+++ 111.cpp
@@ -2,8 +2,13 @@
 111. Minimum Depth of Binary Tree
 
 Given a binary tree, find its minimum depth.
-
 The minimum depth is the number of nodes along the shortest path from the root node down to the nearest leaf node.
+*/
+
+/*
+Time: O(n)
+Space: O(logn)
+Extra space: O(logn)
 */
 
 /**
