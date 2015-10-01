2619398cfe729aebe6abd11800261a0fbc1d66d2
Haochen Liu <liuhaochen1221@hotmail.com>
2015-09-30 23:51:34 -0700

    Update 086-2.cpp

--- 086-2.cpp
+++ 086-2.cpp
@@ -10,6 +10,12 @@ Given 1->4->3->2->5->2 and x = 3,
 return 1->2->2->4->3->5.
 */
 
+/*
+Time: O(n)
+Space: O(1)
+Extra space: O(1)
+*/
+
 /**
  * Definition for singly-linked list.
  * struct ListNode {
