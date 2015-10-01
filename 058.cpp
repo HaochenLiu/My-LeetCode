cf46a6c8afb00b65052e26b28455d1053a57b2e9
Haochen Liu <liuhaochen1221@hotmail.com>
2015-09-30 22:05:38 -0700

    Update 058.cpp

--- 058.cpp
+++ 058.cpp
@@ -2,14 +2,18 @@
 058. Length of Last Word
 
 Given a string s consists of upper/lower-case alphabets and empty space characters ' ', return the length of last word in the string.
-
 If the last word does not exist, return 0.
-
 Note: A word is defined as a character sequence consists of non-space characters only.
 
 For example,
 Given s = "Hello World",
 return 5.
+*/
+
+/*
+Time: O(n)
+Space: O(1)
+Extra space: O(1)
 */
 
 class Solution {
