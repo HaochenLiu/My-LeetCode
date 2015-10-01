2ee2177f054f9becb80f5e9a510ce63c404e0937
Haochen Liu <liuhaochen1221@hotmail.com>
2015-10-01 00:55:54 -0700

    Update 104.cpp

--- 104.cpp
+++ 104.cpp
@@ -6,6 +6,12 @@ Given a binary tree, find its maximum depth.
 The maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
 */
 
+/*
+Time: O(n)
+Space: O(logn)
+Extra space: O(logn)
+*/
+
 /**
  * Definition for binary tree
  * struct TreeNode {
