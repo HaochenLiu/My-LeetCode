8df75578f21462efab92e13593d0a5605efdcef6
Haochen Liu <liuhaochen1221@hotmail.com>
2015-09-30 22:04:28 -0700

    Update 057.cpp

--- 057.cpp
+++ 057.cpp
@@ -2,7 +2,6 @@
 057. Insert Interval
 
 Given a set of non-overlapping intervals, insert a new interval into the intervals (merge if necessary).
-
 You may assume that the intervals were initially sorted according to their start times.
 
 Example 1:
@@ -12,6 +11,12 @@ Example 2:
 Given [1,2],[3,5],[6,7],[8,10],[12,16], insert and merge [4,9] in as [1,2],[3,10],[12,16].
 
 This is because the new interval [4,9] overlaps with [3,5],[6,7],[8,10].
+*/
+
+/*
+Time: O(n)
+Space: O(n)
+Extra space: O(1)
 */
 
 /**
