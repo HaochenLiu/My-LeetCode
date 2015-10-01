865ca114a15b171dc00726cde0d6b858b4d1e7a1
Haochen Liu <liuhaochen1221@hotmail.com>
2015-10-01 01:41:41 -0700

    Update 128.cpp

--- 128.cpp
+++ 128.cpp
@@ -1,7 +1,7 @@
 /*
 128. Longest Consecutive Sequence
-http://www.cnblogs.com/x1957/archive/2013/08/22/3274274.html
 
+http://www.cnblogs.com/x1957/archive/2013/08/22/3274274.html
 Given an unsorted array of integers, find the length of the longest consecutive elements sequence.
 
 For example,
@@ -11,24 +11,34 @@ The longest consecutive elements sequence is [1, 2, 3, 4]. Return its length: 4.
 Your algorithm should run in O(n) complexity.
 */
 
+/*
+Time: O(n)
+Space: O(n)
+Extra space: O(n)
+*/
+
 class Solution {
 public:
     int longestConsecutive(vector<int> &num) {
         unordered_set<int> s;
         int res = 0;
-        for (int i = 0; i < num.size(); ++i)
+        for (int i = 0; i < num.size(); i++) {
             s.insert(num[i]);
-        for (int i = 0; i < num.size() && !s.empty(); ++i)
-        {
-            if (s.find(num[i]) == s.end())
+        }
+        for (int i = 0; i < num.size() && !s.empty(); i++) {
+            if (s.find(num[i]) == s.end()) {
                 continue;
+            }
             int upper = num[i], lower = num[i];
-            while (s.find(upper+1) != s.end())
+            while (s.find(upper + 1) != s.end()) {
                 s.erase(++upper);
-            while (s.find(lower-1) != s.end())
+            }
+            while (s.find(lower - 1) != s.end()) {
                 s.erase(--lower);
-            if (upper != lower)
+            }
+            if (upper != lower) {
                 s.erase(num[i]);
+            }
             res = max(res, upper - lower + 1);
         }
         return res;
