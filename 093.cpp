a2a6e1f71406fe353434a41126a2052175c06d67
Haochen Liu <liuhaochen1221@hotmail.com>
2015-10-01 00:42:08 -0700

    Update 093.cpp

--- 093.cpp
+++ 093.cpp
@@ -9,56 +9,56 @@ Given "25525511135",
 return ["255.255.11.135", "255.255.111.35"]. (Order does not matter)
 */
 
+/*
+Time: O(1)
+Space: O(1)
+Extra space: O(1)
+*/
+
 class Solution {
 private:
-    vector<string> ret;
+    vector<string> res;
     int pos[4];
 public:
-    bool check(string &s, int beg, int end)
-    {
+    bool check(string &s, int beg, int end) {
         string ip(s, beg, end - beg + 1);
-        if (ip.size() == 1)
+        if (ip.size() == 1) {
             return "0" <= ip && ip <= "9";
-        else if (ip.size() == 2)
+        } else if (ip.size() == 2) {
             return "10" <= ip && ip <= "99";
-        else if (ip.size() == 3)
+        } else if (ip.size() == 3) {
             return "100" <= ip && ip <= "255";
-        else
+        } else {
             return false;
+        }
     }
     
-    void dfs(int dep, int maxDep, string &s, int start)
-    {
-        if (dep == maxDep)
-        {
-            if (start == s.size())
-            {
+    void dfs(int dep, int maxDep, string &s, int start) {
+        if (dep == maxDep) {
+            if (start == s.size()) {
                 int beg = 0;
                 string addr;
-                for(int i = 0; i < maxDep; i++)
-                {
+                for(int i = 0; i < maxDep; i++) {
                     string ip(s, beg, pos[i] - beg + 1);
                     beg = pos[i] + 1;
                     addr += i == 0 ? ip : "." + ip;
                 }
-                ret.push_back(addr);    
+                res.push_back(addr);    
             }
             return;
         }
         
-        for(int i = start; i < s.size(); i++)
-            if (check(s, start, i))
-            {
+        for(int i = start; i < s.size(); i++) {
+            if (check(s, start, i)) {
                 pos[dep] = i;
                 dfs(dep + 1, maxDep, s, i + 1);               
             }
+        }
     }
     
     vector<string> restoreIpAddresses(string s) {
-        // Start typing your C/C++ solution below
-        // DO NOT write int main() function
-        ret.clear();
+        res.clear();
         dfs(0, 4, s, 0);
-        return ret;
+        return res;
     }
 };
