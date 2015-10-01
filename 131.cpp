57f2622822fe930926d2028a3ed17eff3c8da0a7
Haochen Liu <liuhaochen1221@hotmail.com>
2015-10-01 01:57:45 -0700

    Update 131.cpp

--- 131.cpp
+++ 131.cpp
@@ -1,9 +1,8 @@
 /*
 131. Palindrome Partitioning
+
 http://www.cnblogs.com/lihaozy/p/3182262.html
-
 Given a string s, partition s such that every substring of the partition is a palindrome.
-
 Return all possible palindrome partitioning of s.
 
 For example, given s = "aab",
@@ -13,6 +12,12 @@ Return
     ["aa","b"],
     ["a","a","b"]
   ]
+*/
+
+/*
+Time: O(n^3)
+Space: O(n)
+Extra space: O(1)
 */
 
 class Solution {
@@ -29,26 +34,26 @@ public:
         return true;
     }
     
-    void findNext(vector<vector<string> > &res, string &s, vector<string> part, int idx) {
+    void findNext(vector<vector<string> > &res, string &s, vector<string> record, int idx) {
         if (idx == s.length()) {
-            res.push_back(part);
+            res.push_back(record);
             return;
         }
         string substr;
         for (int len = s.length() - idx; len > 0; --len) {
             substr = s.substr(idx, len);
             if (isPail(substr)) {
-                part.push_back(substr);
-                findNext(res, s, part, idx + len);
-                part.pop_back();
+                record.push_back(substr);
+                findNext(res, s, record, idx + len);
+                record.pop_back();
             }
         }
     }
     
     vector<vector<string>> partition(string s) {
         vector<vector<string> > res;
-        vector<string> part;
-        findNext(res, s, part, 0);
+        vector<string> record;
+        findNext(res, s, record, 0);
         return res;
     }
 };
