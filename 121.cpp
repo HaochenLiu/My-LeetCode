16438d234a1123900ad2b4dd133ed4f3c4f69126
Haochen Liu <liuhaochen1221@hotmail.com>
2015-10-01 01:30:47 -0700

    Update 121.cpp

--- 121.cpp
+++ 121.cpp
@@ -7,6 +7,12 @@ Say you have an array for which the ith element is the price of a given stock on
 If you were only permitted to complete at most one transaction (ie, buy one and sell one share of the stock), design an algorithm to find the maximum profit.
 */
 
+/*
+Time: O(n)
+Space: O(1)
+Extra space: O(1)
+*/
+
 class Solution {
 public:
     int maxProfit(vector<int> &prices) {
