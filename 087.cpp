3e1e1e76eedd5a11d84ec4ad3fafb93258efde9d
Haochen Liu <liuhaochen1221@hotmail.com>
2015-09-30 23:58:51 -0700

    Update 087.cpp

--- 087.cpp
+++ 087.cpp
@@ -42,28 +42,34 @@ We say that "rgtae" is a scrambled string of "great".
 Given two strings s1 and s2 of the same length, determine if s2 is a scrambled string of s1.
 */
 
+/*
+Time: O(n)
+Space: O(n)
+Extra space: O(n)
+*/
+
 class Solution {
 public:
     bool isScramble(string s1, string s2) {
         if(s1 == s2) return true;
         if(s1.size() != s2.size()) return false;
-        int charset[26] = {0};
+        vector<int> charset(26, 0);
         for(int i = 0 ; i < s1.size() ; ++i) {
-            charset[s1[i]-'a']++;
+            charset[s1[i] - 'a']++;
         }
         for(int i = 0 ; i < s2.size() ; ++i) {
-            charset[s2[i]-'a']--;
+            charset[s2[i] - 'a']--;
         }
         for(int i = 0 ; i < 26 ; ++i) {
             if(charset[i] != 0) return false;
         }        
         bool result = false;
         for(int i = 1 ; i < s1.size() ; ++i) {
-            result = isScramble(s1.substr(0,i) , s2.substr(0,i)) && isScramble(s1.substr(i) , s2.substr(i));
+            result = isScramble(s1.substr(0, i) , s2.substr(0, i)) && isScramble(s1.substr(i) , s2.substr(i));
             if(result) {
                 return true;
             }
-            result = isScramble(s1.substr(0,i) , s2.substr(s1.size()-i,i)) && isScramble(s1.substr(i) , s2.substr(0 , s1.size()-i));
+            result = isScramble(s1.substr(0, i) , s2.substr(s1.size() - i, i)) && isScramble(s1.substr(i) , s2.substr(0 , s1.size() - i));
             if(result) {
                 return true;
             }
