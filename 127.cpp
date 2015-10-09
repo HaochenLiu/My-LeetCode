ab01f1c9b410df23991b35e9a738a70a1d9c748e
Haochen Liu <liuhaochen1221@hotmail.com>
2015-10-09 13:01:03 -0700

    Update 127.cpp

--- 127.cpp
+++ 127.cpp
@@ -26,29 +26,29 @@ Note:
 class Solution {
 public:
     int ladderLength(string beginWord, string endWord, unordered_set<string>& wordList) {
-        if (beginWord.size() != endWord.size()) return 0;
-        if (beginWord.empty() || endWord.empty()) return 1;
-        if (wordList.size() == 0) return 0;
+        if(beginWord.size() != endWord.size()) return 0;
+        if(beginWord.empty() || endWord.empty()) return 1;
+        if(wordList.size() == 0) return 0;
 
         int distance = 1;
         queue<string> queToPush;
         queue<string> queToPop;
         queToPop.push(beginWord);
-        while (wordList.size() > 0 && !queToPop.empty()) {
-            while (!queToPop.empty()) {
+        while(wordList.size() > 0 && !queToPop.empty()) {
+            while(!queToPop.empty()) {
                 string str(queToPop.front());
                 queToPop.pop();
-                for (int i = 0; i < str.size(); i++) {
-                    for (char j = 'a'; j <= 'z'; j++) {
-                        if (j == str[i]) {
+                for(int i = 0; i < str.size(); i++) {
+                    for(char j = 'a'; j <= 'z'; j++) {
+                        if(j == str[i]) {
                             continue;
                         }
                         char temp = str[i];
                         str[i] = j;
-                        if (str == endWord) {
+                        if(str == endWord) {
                             return distance + 1;
                         }
-                        if (wordList.find(str) != wordList.end()) {
+                        if(wordList.find(str) != wordList.end()) {
                             queToPush.push(str);
                             wordList.erase(str);
                         }
