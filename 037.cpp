c6cc004349119be36e5e9b186c0c7311f8a33b39
Haochen Liu <liuhaochen1221@hotmail.com>
2015-10-09 12:03:58 -0700

    Update 037.cpp

--- 037.cpp
+++ 037.cpp
@@ -16,52 +16,63 @@ e is number of empty spaces in sudoku.
 */
 
 class Solution {
-public:
-    bool isValidSudoku(vector<vector<char> > &board, int row, int col) {
-        for(int i = 0; i < 9; i++) {
-            if(i != row && board[row][col] == board[i][col]) {
+private:
+    int N;
+
+    bool isValid(vector<vector<char>>& board, int x, int y) {
+        //check line
+        for(int j = 0; j < N; j++) {
+            if(j != y && board[x][j] == board[x][y]) {
                 return false;
             }
         }
         
-        for(int i = 0; i < 9; i++) {
-            if(i != col && board[row][col] == board[row][i]) {
+        //check colume
+        for(int i = 0; i < N; i++) {
+            if(i != x && board[i][y] == board[x][y]) {
                 return false;
             }
         }
         
-        for(int i = row / 3 * 3; i < row / 3 * 3 + 3; i++) {
-            for(int j = col /3 * 3; j < col /3 * 3 + 3; j++) {
-                if(i != row && j != col && board[row][col] == board[i][j]) {
+        //check block
+        for(int i = x / 3 * 3; i < x / 3 * 3 + 3; i++) {
+            for(int j = y / 3 * 3; j < y / 3 * 3 + 3; j++) {
+                if((i != x || j != y) && board[i][j] == board[x][y]) {
                     return false;
                 }
             }
         }
+
         return true;
     }
-    
-    bool solve(vector<vector<char> > &board) {
-        for(int i = 0; i < 9; i++) {
-            for(int j = 0; j < 9; j++) {
-                if(board[i][j] == '.') {
-                    for(int k = 1; k <= 9; k++) {
-                        board[i][j] = '0' + k;
-                        if(isValidSudoku(board, i, j)) {
-                            if(solve(board)) {
-                                return true;
-                            }
+
+    bool solveSudokuRecur(vector<vector<char>>& board) {
+        for(int i = 0; i < N; i++) {
+            for(int j = 0; j < N; j++) {
+                if(board[i][j] != '.') {
+                    continue;
+                }
+                for(int k = 1; k <= 9; k++) {
+                    board[i][j] = k + '0';
+                    if(isValid(board, i, j)) {
+                        if(solveSudokuRecur(board)) {
+                            return true;
                         }
                     }
-                    board[i][j] = '.';
-                    return false;
                 }
+                board[i][j] = '.';
+                return false;
             }
         }
+
         return true;
     }
     
-    void solveSudoku(vector<vector<char> > &board) {
-        solve(board);
+public:
+    void solveSudoku(vector<vector<char>>& board) {
+        if(board.size() != 9 || board[0].size() != 9) return;
+        N = board.size();
+        solveSudokuRecur(board);
         return;
     }
 };
