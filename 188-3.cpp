2f9e08e26c3697123a2290697af922280f9e4816
Haochen Liu <liuhaochen1221@hotmail.com>
2015-10-17 10:34:51 -0700

    Update and rename 188-2.cpp to 188-3.cpp

--- 188-3.cpp
+++ 188-3.cpp
@@ -11,8 +11,11 @@ Special thanks to @Freezen for adding this problem and creating all test cases.
 http://blog.csdn.net/foreverling/article/details/43911309
 */
 
-// Time： O(nk)
-// Space: O(nk)
+/*
+Time： O(nk)
+Space: O(nk)
+*/
+
 class Solution {
 public:
     int maxProfit(int k, vector<int> &prices) {
