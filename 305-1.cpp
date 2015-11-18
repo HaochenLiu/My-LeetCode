2f30adac5e426b267c128c7004775a5aa217470f
Haochen Liu <liuhaochen1221@hotmail.com>
2015-11-17 17:09:12 -0800

    Update and rename 305.cpp to 305-1.cpp

--- 305-1.cpp
+++ 305-1.cpp
@@ -38,9 +38,9 @@ Operation #4: addLand(2, 1) turns the water at grid[2][1] into a land.
 
 We return the result as an array: [1, 1, 2, 3]
 
-Update: (2015/11/13)
-If you are using Java, the parameter List<int[]> positions had been changed to int[][] positions. Please click on the reload button above the code editor
-to reset the code definition.
+Challenge:
+
+Can you do it in time complexity O(k log mn), where k is the length of the positions?
 */
 
 class Solution {
@@ -48,41 +48,34 @@ public:
     vector<int> numIslands2(int m, int n, vector<pair<int, int>>& positions) {
         vector<int> res;
         if(n == 0 || m == 0) return res;
-        vector<vector<int>> grid(m, vector<int>(n, 0));
         vector<int> father(n * m, -1);
         int sz = positions.size();
         for(int i = 0; i < sz; i++) {
             int x = positions[i].first;
             int y = positions[i].second;
-            grid[x][y] = 1;
             int t = x * n + y;
             int ft = find(father, t);
             if(i == 0) {
                 res.push_back(1);
                 continue;
             }
-            unordered_set<int> s;
-            for(int k = 0; k < 4; k++) {
-                int nx = x + dx[k];
-                int ny = y + dy[k];
-                int nt = nx * n + ny;
-                if(!inBound(nx, ny, m, n)) continue;
-                if(grid[nx][ny] == 0) continue;
-                s.insert(find(father, nt));
-            }
-            res.push_back(res.back() + 1 - s.size());
 
+            int cnt = res.back() + 1;
             for(int k = 0; k < 4; k++) {
                 int nx = x + dx[k];
                 int ny = y + dy[k];
                 int nt = nx * n + ny;
                 if(!inBound(nx, ny, m, n)) continue;
-                if(grid[nx][ny] == 0) continue;
+                if(father[nt] == -1) continue;
                 ft = find(father, t);
                 int fnt = find(father, nt);
-                father[ft] = min(ft, fnt);
-                father[fnt] = min(ft, fnt);
+                if(ft != fnt) {
+                    cnt--;
+                    father[ft] = min(ft, fnt);
+                    father[fnt] = min(ft, fnt);
+                }
             }
+            res.push_back(cnt);
         }
 
         return res;
@@ -102,7 +95,7 @@ private:
         while(father[r] != r) {
             r = father[r];
         }
-        
+
         int f = a;
         int p;
         while(f != r) {
