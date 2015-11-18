b16ca241c27541bf4740ad1cf6b51a3c11db5419
Haochen Liu <liuhaochen1221@hotmail.com>
2015-11-18 00:55:10 -0800

    Update 304.cpp

--- 304.cpp
+++ 304.cpp
@@ -1,5 +1,5 @@
 /*
-Range Sum Query 2D - Immutable
+304. Range Sum Query 2D - Immutable
 
 Given a 2D matrix matrix, find the sum of the elements inside the rectangle defined by its upper left corner (row1, col1) and lower right corner (row2, col2).
 
