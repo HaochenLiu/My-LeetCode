71f20525874807e45ce6f81dd3ae9cf073237a2d
Haochen Liu <liuhaochen1221@hotmail.com>
2015-11-22 15:20:52 -0800

    Update 308.cpp

--- 308.cpp
+++ 308.cpp
@@ -24,88 +24,148 @@ You may assume the number of calls to update and sumRegion function is distribut
 You may assume that row1 ≤ row2 and col1 ≤ col2.
 */
 
-class SegmentTree {
+class KDTree {
 public:
-    int low;
-    int high;
+    int depth;
     int sum;
-    SegmentTree* left;
-    SegmentTree* right;
+    int row1;
+    int row2;
+    int col1;
+    int col2;
+    KDTree* left;
+    KDTree* right;
 };
 
-SegmentTree* createSegmentTree(vector<int>& v, int l, int h) {
-    SegmentTree* t = new SegmentTree();
-    t->low = l;
-    t->high = h;
-    
-    if(l == h) {
-        t->sum = v[l];
+KDTree* createKDTree(vector<vector<int>> &matrix, int d, int r1, int r2, int c1, int c2) {
+    KDTree* t = new KDTree();
+    t->depth = d;
+    t->row1 = r1;
+    t->row2 = r2;
+    t->col1 = c1;
+    t->col2 = c2;
+    if(r1 == r2 && c1 == c2) {
+        t->sum = matrix[r1][c1];
         t->left = NULL;
         t->right = NULL;
         return t;
     }
     
-    t->left = createSegmentTree(v, l, l + (h - l) / 2);
-    t->right = createSegmentTree(v, l + (h - l) / 2 + 1, h);
-    t->sum = t->left->sum + t->right->sum;
+    if(d % 2 == 1) {
+        if(r1 == r2) {
+            t->left = createKDTree(matrix, d + 1, r1, r2, c1, c2);
+            t->right = NULL;
+            t->sum = t->left->sum;
+        } else {
+            t->left = createKDTree(matrix, d + 1, r1, r1 + (r2 - r1) / 2, c1, c2);
+            t->right = createKDTree(matrix, d + 1, r1 + (r2 - r1) / 2 + 1, r2, c1, c2);
+            t->sum = t->left->sum + t->right->sum;
+        }
+    } else {
+        if(c1 == c2) {
+            t->left = createKDTree(matrix, d + 1, r1, r2, c1, c2);
+            t->right = NULL;
+            t->sum = t->left->sum;
+        } else {
+            t->left = createKDTree(matrix, d + 1, r1, r2, c1, c1 + (c2 - c1) / 2);
+            t->right = createKDTree(matrix, d + 1, r1, r2, c1 + (c2 - c1) / 2 + 1, c2);
+            t->sum = t->left->sum + t->right->sum;
+        }
+    }
+
     return t;
 }
 
-class NumArray {
+class NumMatrix {
 private:
-    SegmentTree* t = NULL;
-    vector<int> v;
+    KDTree* t = NULL;
+    vector<vector<int>> v;
 public:
-    NumArray(vector<int> &nums) {
-        if(nums.empty()) {
+    NumMatrix(vector<vector<int>> &matrix) {
+        if(matrix.empty() || matrix[0].empty()) {
             t = NULL;
         } else {
-            t = createSegmentTree(nums, 0, nums.size() - 1);
+            t = createKDTree(matrix, 1, 0, matrix.size() - 1, 0, matrix[0].size() - 1);
         }
-        v = nums;
+        v = matrix;
         return;
     }
 
-    void update(int i, int val) {
-        if(i < 0 || i >= v.size()) return;
-        if(v[i] == val) return;
-        int delta = val - v[i];
-        v[i] = val;
-        SegmentTree* node = t;
-        while(true) {
-            if(i >= node->low && i <= node->high) {
-                node->sum += delta;
-            }
-            if(node->low == node->high) {
-                break;
-            }
-            if(i <= node->left->high) {
-                node = node->left;
+    void update(int row, int col, int val) {
+        if(row < 0 || row >= v.size()) return;
+        if(col < 0 || col >= v[0].size()) return;
+        if(v[row][col] == val) return;
+
+        int delta = val - v[row][col];
+        v[row][col] = val;
+        KDTree* node = t;
+        while(node) {
+            node->sum += delta;
+            if(node->depth % 2 == 1) {
+                if(node->row1 == node->row2) {
+                    node = node->left;
+                } else {
+                    if(row < node->right->row1) {
+                        node = node->left;
+                    } else {
+                        node = node->right;
+                    }
+                }
             } else {
-                node = node->right;
+                if(node->col1 == node->col2) {
+                    node = node->left;
+                } else {
+                    if(col < node->right->col1) {
+                        node = node->left;
+                    } else {
+                        node = node->right;
+                    }
+                }
             }
         }
-        return;
     }
 
-    int sumRangeRecur(SegmentTree* node, int i, int j) {
-        if(node->low == i && node->high == j) return node->sum;
-        if(i > node->left->high) return sumRangeRecur(node->right, i, j);
-        if(j < node->right->low) return sumRangeRecur(node->left, i, j);
-        return sumRangeRecur(node->left, i, node->left->high) + sumRangeRecur(node->right, node->right->low, j);
+    int sumRegion(int row1, int col1, int row2, int col2) {
+        if(row1 > row2) return 0;
+        if(row1 < 0) return 0;
+        if(row2 >= v.size()) return 0;
+        if(col1 > col2) return 0;
+        if(col1 < 0) return 0;
+        if(col2 >= v[0].size()) return 0;
+        if(row1 == row2 && col1 == col2) return v[row1][col1];
+
+        return sumRegionRecur(t, row1, row2, col1, col2);
     }
 
-    int sumRange(int i, int j) {
-        if(i > j) return 0;
-        if(i < 0) return 0;
-        if(j >= v.size()) return 0;
-        if(i == j) return v[i];
-        return sumRangeRecur(t, i, j);
+    int sumRegionRecur(KDTree* node, int row1, int row2, int col1, int col2) {
+        if(node == NULL) return 0;
+        if(row2 < node->row1) return 0;
+        if(row1 > node->row2) return 0;
+        if(col2 < node->col1) return 0;
+        if(col1 > node->col2) return 0;
+        if(node->row1 == row1 && node->row2 == row2 && node->col1 == col1 && node->col2 == col2) return node->sum;
+
+        if(node->depth % 2 == 1) {
+            if(node->row1 == node->row2) {
+                return sumRegionRecur(node->left, row1, row2, col1, col2);
+            } else {
+                if(row1 > node->left->row2) return sumRegionRecur(node->right, row1, row2, col1, col2);
+                if(row2 < node->right->row1) return sumRegionRecur(node->left, row1, row2, col1, col2);
+                return sumRegionRecur(node->left, row1, node->left->row2, col1, col2) + sumRegionRecur(node->right, node->right->row1, row2, col1, col2);
+            }
+        } else {
+            if(node->col1 == node->col2) {
+                return sumRegionRecur(node->left, row1, row2, col1, col2);
+            } else {
+                if(col1 > node->left->col2) return sumRegionRecur(node->right, row1, row2, col1, col2);
+                if(col2 < node->right->col1) return sumRegionRecur(node->left, row1, row2, col1, col2);
+                return sumRegionRecur(node->left, row1, row2, col1, node->left->col2) + sumRegionRecur(node->right, row1, row2, node->right->col1, col2);
+            }
+        }
     }
 };
 
-// Your NumArray object will be instantiated and called as such:
-// NumArray numArray(nums);
-// numArray.sumRange(0, 1);
-// numArray.update(1, 10);
-// numArray.sumRange(1, 2);
+// Your NumMatrix object will be instantiated and called as such:
+// NumMatrix numMatrix(matrix);
+// numMatrix.sumRegion(0, 1, 2, 3);
+// numMatrix.update(1, 1, 10);
+// numMatrix.sumRegion(1, 2, 3, 4);
