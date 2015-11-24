400cdba46871facc4c7c4cab088e2cff531f8389
Haochen Liu <liuhaochen1221@hotmail.com>
2015-11-24 01:26:32 -0800

    Update 063.cpp

--- 063.cpp
+++ 063.cpp
@@ -27,41 +27,38 @@ Extra space: O(nm)
 
 class Solution {
 public:
-    int uniquePathsWithObstacles(vector<vector<int> > &obstacleGrid) {
-        int m = obstacleGrid.size();
-        int n = obstacleGrid[0].size();
-        vector<vector<int> > f(m, vector<int>(n));
+    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+        if(obstacleGrid.empty() || obstacleGrid[0].empty()) return 0;
+        int n = obstacleGrid.size();
+        int m = obstacleGrid[0].size();
+        vector<vector<int>> dp(n, vector<int>(m, 0));
         
-        bool bad = false;
         for(int i = 0; i < n; i++) {
-            if(obstacleGrid[0][i] == 1 || bad) {
-                f[0][i] = 0;
-                bad = true;
+            if(obstacleGrid[i][0] != 1) {
+                dp[i][0] = 1;
             } else {
-                f[0][i] = 1;
+                break;
             }
         }
             
-        bad = false;
-        for(int i = 0; i < m; i++) {
-            if(obstacleGrid[i][0] == 1 || bad) {
-                f[i][0] = 0;
-                bad = true;
+        for(int j = 0; j < m; j++) {
+            if(obstacleGrid[0][j] != 1) {
+                dp[0][j] = 1;
             } else {
-                f[i][0] = 1;
+                break;
             }
         }
             
-        for(int i = 1; i < m; i++) {
-            for(int j = 1; j < n; j++) {
+        for(int i = 1; i < n; i++) {
+            for(int j = 1; j < m; j++) {
                 if(obstacleGrid[i][j] == 1) {
-                    f[i][j] = 0;
+                    dp[i][j] = 0;
                 } else {
-                    f[i][j] = f[i-1][j] + f[i][j-1];
+                    dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
                 }
             }
         }
         
-        return f[m-1][n-1];
+        return dp[n - 1][m - 1];
     }
 };
