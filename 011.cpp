bb243f0e534508991578bb24c2ca29a6a1e60cb9
Haochen Liu <liuhaochen1221@hotmail.com>
2015-11-24 01:04:27 -0800

    Update 011.cpp

--- 011.cpp
+++ 011.cpp
@@ -17,6 +17,7 @@ public:
         int i = 0;
         int j = height.size() - 1;
         int res = 0;
+
         while(i < j) {
             res = max(res, min(height[i], height[j]) * (j - i));
             if(height[i] < height[j]) {
