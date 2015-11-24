2fa5867a9d0d87ef95be78944c7c7752f0c86fa5
Haochen Liu <liuhaochen1221@hotmail.com>
2015-11-24 01:13:10 -0800

    Update 011.cpp

--- 011.cpp
+++ 011.cpp
@@ -15,7 +15,8 @@ class Solution {
 public:
     int maxArea(vector<int>& height) {
         int i = 0;
-        int j = height.size() - 1;
+        int n = height.size();
+        int j = n - 1;
         int res = 0;
 
         while(i < j) {
