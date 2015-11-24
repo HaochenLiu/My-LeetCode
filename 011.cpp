bace147b5e6d06c104858403925d9a626ef07126
Haochen Liu <liuhaochen1221@hotmail.com>
2015-11-24 01:04:12 -0800

    Update 011.cpp

--- 011.cpp
+++ 011.cpp
@@ -13,23 +13,19 @@ Extra space: O(1)
 
 class Solution {
 public:
-    int maxArea(vector<int> &height) {
+    int maxArea(vector<int>& height) {
         int i = 0;
         int j = height.size() - 1;
         int res = 0;
-        while(i < j)
-        {
-            int temp = min(height[i], height[j]) * (j - i);
-            if(temp > res) {
-                res = temp;
-            }
-
+        while(i < j) {
+            res = max(res, min(height[i], height[j]) * (j - i));
             if(height[i] < height[j]) {
                 i++;
             } else {
                 j--;
             }
         }
+
         return res;
     }
 };
