1a28128791a1a95934f676e9bfe8087931fd1779
Haochen Liu <liuhaochen1221@hotmail.com>
2016-01-07 00:05:18 -0800

    Update 001.cpp

--- 001.cpp
+++ 001.cpp
@@ -22,7 +22,7 @@ public:
         unordered_map<int, int> m;
         vector<int> res;
         for(int i = 0; i < n; i++) {
-            if (m[target - numbers[i]] != 0) {
+            if(m.find(target - numbers[i]) != m.end()) {
                 res.push_back(m[target - numbers[i]]);
                 res.push_back(i + 1);
                 return res;
