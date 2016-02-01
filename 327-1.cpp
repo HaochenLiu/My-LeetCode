208c34b02b6e7a8fea69521009317a747ba0f37e
Haochen Liu <liuhaochen1221@hotmail.com>
2016-02-01 09:51:51 -0800

    Update 327-1.cpp

--- 327-1.cpp
+++ 327-1.cpp
@@ -1,5 +1,5 @@
 /*
-327. Count of Range Sum My Submissions Question
+327. Count of Range Sum
 
 Given an integer array nums, return the number of range sums that lie in [lower, upper] inclusive.
 Range sum S(i, j) is defined as the sum of the elements in nums between indices i and j (i ≤ j), inclusive.
