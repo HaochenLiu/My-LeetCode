b1fafed95ace1ce2eee8e710ec9fc20e9f79d8e5
Haochen Liu <liuhaochen1221@hotmail.com>
2016-06-01 13:43:26 -0700

    Update 349.cpp

--- 349.cpp
+++ 349.cpp
@@ -18,21 +18,17 @@ public:
         unordered_map<int,int> map;
         vector<int> result;
 
-        for(int i=0;i<nums1.size();i++)
-        {
+        for(int i=0;i<nums1.size();i++) {
             map[nums1[i]]++;
         }
 
-        for(int i=0;i<nums2.size();i++)
-        {
-            if(map[nums2[i]] > 0)
-            {
+        for(int i=0;i<nums2.size();i++) {
+            if(map[nums2[i]] > 0) {
                 result.push_back(nums2[i]);
                 map[nums2[i]] = 0;
             }
         }
 
         return result;
-
     }
 };
